16e59f2d6df4863a8dd56ab459b90d89b37930e6
AlexBoronin <acivinel@mail.ru>
2023-04-06 18:42:28 +0300

    feat: not scrolly when popup

--- shelter/scripts/index.js
+++ shelter/scripts/index.js
@@ -1,4 +1,4 @@
-window.onload = function (){
+window.onload = function () {
 
     const header = document.querySelector('.header');
     window.onscroll = () => {
@@ -9,34 +9,44 @@ window.onload = function (){
         }
     };
     const burger = document.getElementById('burger');
-    const menu = document.getElementsByClassName('menu')
-    const menuItem = document.getElementsByClassName('menu__item')
+    const menu = document.getElementsByClassName('menu');
+    const popup = document.querySelector('.popup');
+    const body = document.querySelector('body')
+    const scrollY = document.body.style.top;
+    document.body.style.position = '';
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
 
     burger.addEventListener('click', () => {
         burger.classList.toggle('burger-open');
-        menu[0].classList.toggle('open')
-        })
+        menu[0].classList.toggle('open');
+        popup.classList.toggle('popup-active');
+        if(body.style.position !== 'fixed'){
+            body.setAttribute('style',"position: fixed");
+        }else {
+            body.setAttribute('style',"position: unset");
+        };
+    })
 
     document.querySelectorAll('#menu *').forEach((item) => {
         item.onclick = () => {
             menu[0].classList.toggle('open')
             burger.classList.toggle('burger-open');
+            popup.classList.toggle('popup-active')
+            body.style.position = 'unset';
         }
-     })
-
-
-    document.addEventListener('click',(el) => {
-        if(el.target !== burger){
-            menu[0].classList.remove('open')
-            burger.classList.remove('burger-open');
-        }
-        // else {
-        //     // burger.addEventListener('click', () => {
-        //         burger.classList.toggle('burger-open');
-        //         menu[0].classList.toggle('open')
-        //     // })
-        // }
     })
+
+
+    popup.addEventListener('click', () => {
+        menu[0].classList.remove('open')
+        burger.classList.remove('burger-open');
+        popup.classList.toggle('popup-active')
+        body.style.position = 'unset';
+    })
+
+
 
 
 }
