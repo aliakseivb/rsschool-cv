b27c60f4ec5a0ddb6c7a26e6ade2856966ed98e2
AlexBoronin <acivinel@mail.ru>
2023-04-07 13:21:44 +0300

    feat: add json

--- shelter/scripts/index.js
+++ shelter/scripts/index.js
@@ -1,13 +1,13 @@
 window.onload = function () {
 
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-        if (window.scrollY > 50) {
-            header.classList.add('header__active');
-        } else {
-            header.classList.remove('header__active');
-        }
-    };
+    // const header = document.querySelector('.header');
+    // window.onscroll = () => {
+    //     if (window.scrollY > 50) {
+    //         header.classList.add('header__active');
+    //     } else {
+    //         header.classList.remove('header__active');
+    //     }
+    // };
     const burger = document.getElementById('burger');
     const menu = document.getElementsByClassName('menu');
     const popup = document.querySelector('.popup');
@@ -22,11 +22,12 @@ window.onload = function () {
         burger.classList.toggle('burger-open');
         menu[0].classList.toggle('open');
         popup.classList.toggle('popup-active');
-        if(body.style.position !== 'fixed'){
-            body.setAttribute('style',"position: fixed");
-        }else {
-            body.setAttribute('style',"position: unset");
-        };
+        if (body.style.position !== 'fixed') {
+            body.setAttribute('style', "position: fixed");
+        } else {
+            body.setAttribute('style', "position: unset");
+        }
+        ;
     })
 
     document.querySelectorAll('#menu *').forEach((item) => {
@@ -46,7 +47,82 @@ window.onload = function () {
         body.style.position = 'unset';
     })
 
-
-
-
 }
+
+
+let pets = [];
+Promise.all([fetch('../src/pets.json'),]
+)
+    .then(async ([petsResponse]) => {
+        const petsJson = await petsResponse.json();
+        return [petsJson];
+    })
+    .then(res => {
+        pets = res[0];
+        console.log(pets);
+    });
+// let url = '../src/pets.json'
+
+// const getData = function (url) {
+//     new Promise((resolve, reject) =>
+//     fetch(url)
+//         .then(response => console.log(response.json()))
+//         .then(json => console.log(resolve(json)))
+//         .catch(error => reject(error))
+//     )
+//
+// }
+// getData(fetch(url))
+// import data from "../src/pets.json"
+// let data
+// let pets = []
+//
+// async function getDAta() {
+//     const url = '../src/pets.json';
+//     const res = await fetch(url);
+//     data = await res.json();
+//     console.log(data)
+//     data.forEach((item) => pets.push(item));
+//     return pets
+// }
+// console.log(getDAta())
+// import pets from '../src/pets.json'
+// console.log(pets[0].name)
+// const my = JSON.parse(pets)
+// console.log(my)
+
+// async function getData (){
+//     res = await fetch('../src/pets.json');
+//     console.log(res)
+//     data = await JSON.parse(res);
+//     console.log(data)
+//     // data.forEach((item) => pets.push(item));
+//     // console.log(pets)
+//     // console.log(pets.length)
+//     // return pets
+// }
+// console.log(getData());
+
+// function parseJSON(jsonString) {
+//     return new Promise((resolve, reject) => {
+//         try {
+//             const jsonObject = JSON.parse(jsonString);
+//             resolve(jsonObject);
+//         } catch (error) {
+//             reject(error);
+//         }
+//     });
+// }
+// async function run() {
+//     const jsonString = pets[0];
+//     try {
+//         // let res = await fetch(jsonString);
+//         const jsonObject = await parseJSON(data);
+//         console.log(jsonObject.name); // выводит "John"
+//         console.log(jsonObject.type); // выводит 30
+//         // console.log(jsonObject.city); // выводит "New York"
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+// console.log(run());
